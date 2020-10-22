148404b4aea28962de763f9480b128217e3a4a6d
Matt Deakyne <m.d@ku.edu>
2020-10-22 13:36:49 -0500

    Updated main with lti launch keys

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/launch")
-async def launch(**kwargs):
-    for key, value in kwargs.items():
-        print ("%s == %s" %(key, value))
-    return kwargs
-
+async def launch(iss, login_hint, target_link_uri, lti_message_hint):
+    print(f"ISS is {iss}")
+    print(f"Login hint is {login_hint}")
+    print(f"Target link uri is {target_link_uri}")
+    print(f"lti_message_hint is {lti_message_hint}")
+    return {'iss':iss, 
+            'login_hint':login_hint,
+            'target_link_uri':target_link_uri,
+            'lti_message_hint':lti_message_hint}
+    
