a9799f7b98206044813e4b381b7e16523b006db6
Matt Deakyne <m.d@ku.edu>
2020-10-28 09:52:44 -0500

    Updated redirect

--- tool.py
+++ tool.py
@@ -46,7 +46,17 @@ async def launch(request):
     
     wp.add(jp.P(text=f"Redirecting to {url}"))
     print(url)
-    wp.redirect = url
+    """
+    https://bbtest2.cc.ku.edu/webapps/blackboard/execute/blti/launchPlacement?cmd=authenticate&
+    client_id=8b391e79-ab66-4f8a-9fa2-027782f7a598&
+    login_hint=https%253A%252F%252Fbbtest2.cc.ku.edu%252Fwebapps%252Fblackboard%252Fexecute%252Fblti%252FlaunchPlacement%253Fcmd%253Dauthenticate%2C884f23954de74294bff1ab9d1a8a43a7&
+    lti_message_hint=eyJmcm9tVWx0cmEiOmZhbHNlLCJwb3NpdGlvbiI6LTEsInRhcmdldExpbmtVcmwiOiJodHRwczovL2JibHRpLmhlcm9rdWFwcC5jb20iLCJwbGFjZW1lbnRJZCI6Il8zNjRfMSIsInRhcmdldE92ZXJyaWRlIjpudWxsLCJmcm9tR3JhZGVDZW50ZXIiOmZhbHNlLCJvcGVuTmV3V2luZG93IjpmYWxzZSwiZGVlcExpbmtMYXVuY2giOnRydWUsImNvdXJzZUlkIjoiXzIzNzYxNV8xIiwiY29udGVudElkIjoiXzYyMDg0M18xIn0%3D&nonce=fc5fdc6d-5dd6-47f4-b2c9-5d1216e9b771
+    &redirect_uri=https%3A%2F%2Fbbltiapi.herokuapp.com%2F
+    &response_type=id_token
+    &scope=openid
+    &state=a%20unique%20value
+    """
+    wp.redirect = 'https://www.google.com'
     
     return wp
    
